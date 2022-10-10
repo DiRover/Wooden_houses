0a498ade5eadbbad97abcf02840edfe92b4eaa8e
Rovenskiy <arhitec@bk.ru>
2022-10-10 22:14:40 +0300

    header v1.0

--- components/Header/CustomMenu.tsx
+++ components/Header/CustomMenu.tsx
@@ -1,87 +1,91 @@
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import { FC, memo, MouseEvent, useState } from 'react';
+import Typography from '@mui/material/Typography';
+import {FC, memo, MouseEvent, useState} from 'react';
 
 interface Props {
-  text: string;
+    text: string;
 }
 
+const buttonStyleProps = {
+    zIndex: 1,
+    display: 'inline-block',
+    height: 'auto',
+    alignSelf: 'center',
+    padding: 0,
+    textTransform: 'none',
+    // color: 'hotpink',
+    // '&:hover': {
+    //     backgroundColor: 'white',
+    // },
+};
+
 const CustomMenu: FC<Props> = memo(({text}) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLButtonElement>(null);
-  const [open, setOpen] = useState<boolean>(false);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLButtonElement>(null);
 
-  const handleOpenMenu = (e: MouseEvent<HTMLButtonElement>) => {
-    setOpen(true);
-    setAnchorEl(e.currentTarget);
-  };
+    const [open, setOpen] = useState<boolean>(false);
 
-  const handleCloseMenu = () => {
-    setOpen(false);
-    setAnchorEl(null);
-  };
+    const handleOpenMenu = (e: MouseEvent<HTMLButtonElement>) => {
+        setOpen(true);
+        setAnchorEl(e.currentTarget);
+    };
 
-  return (
-    <div>
-      <Button
-        onMouseEnter={handleOpenMenu}
-        sx={{
-          zIndex: 1,
-          textTransform: 'none',
-          color: "hotpink",
-          "&:hover": {
-            backgroundColor: "white"
-          }
-        }}
-        disableRipple
-      >
-        <div>{text}</div>
-      </Button>
-      <Menu
-        anchorEl={anchorEl}
-        open={open}
-        onClick={handleCloseMenu}
-        MenuListProps={{
-          onMouseLeave: handleCloseMenu,
-          sx: { padding: 0, color: "yellow" },
-          autoFocus: true
-        }}
-        sx={{ padding: 0 }}
-      >
-        <MenuItem
-          autoFocus={true}
-          sx={{
-            "&:hover": {
-              color: "red",
-              backgroundColor: "pink"
-            }
-          }}
-        >
-          Profile
-        </MenuItem>
-        <MenuItem
-          sx={{
-            "&:hover": {
-              color: "red",
-              backgroundColor: "blue"
-            }
-          }}
-        >
-          My account
-        </MenuItem>
-        <MenuItem
-          sx={{
-            "&:hover": {
-              color: "red",
-              backgroundColor: "green"
-            }
-          }}
-        >
-          Logout
-        </MenuItem>
-      </Menu>
-    </div>
-  );
+    const handleCloseMenu = () => {
+        setOpen(false);
+        setAnchorEl(null);
+    };
+
+    return (
+        <>
+            <Button onMouseEnter={handleOpenMenu} size="small" sx={buttonStyleProps} disableRipple>
+                <Typography>{text}</Typography>
+            </Button>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClick={handleCloseMenu}
+                MenuListProps={{
+                    onMouseLeave: handleCloseMenu,
+                    sx: {padding: 0, color: 'yellow'},
+                    autoFocus: true,
+                }}
+                sx={{padding: 0}}
+            >
+                <MenuItem
+                    autoFocus={true}
+                    sx={{
+                        '&:hover': {
+                            color: 'red',
+                            backgroundColor: 'pink',
+                        },
+                    }}
+                >
+                    Profile
+                </MenuItem>
+                <MenuItem
+                    sx={{
+                        '&:hover': {
+                            color: 'red',
+                            backgroundColor: 'blue',
+                        },
+                    }}
+                >
+                    My account
+                </MenuItem>
+                <MenuItem
+                    sx={{
+                        '&:hover': {
+                            color: 'red',
+                            backgroundColor: 'green',
+                        },
+                    }}
+                >
+                    Logout
+                </MenuItem>
+            </Menu>
+        </>
+    );
 });
 
 export default CustomMenu;
